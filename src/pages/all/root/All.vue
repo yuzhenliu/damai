6539ce4daf2a2dbd9c1b6c010f8a13f402d99acc
yuzhenliu <1109763686@qq.com>
2019-11-13 21:29:35 +0800

    改了一下全部页面的滚动布局

--- src/pages/all/root/All.vue
+++ src/pages/all/root/All.vue
@@ -5,8 +5,9 @@
       <app-header title="全部" hasBack>
         <van-icon name="search" slot="right-btn" class="right-btn" />
       </app-header>
-      <!-- <div class="content"> -->
-        <app-scroll class="content">
+
+      <div class="content">
+        <!-- 头部 -->
         <van-tabs
           color="#ff1268"
           title-inactive-color="#111"
@@ -14,47 +15,39 @@
           :swipeable="true"
           line-height="0"
           @click="clickAction"
-          :sticky="true"
         >
           <van-tab
-            v-for="item in classifyArr"
-            :title="item.name"
-            :key="item.classify_id"
-            :border="false"
-          >
-          <!-- <h1>城市选择</h1> -->
-          <!-- <app-scroll class="scrollContent"> -->
-            <app-good-item
-              v-for="good in goodsListArr"
-              :key="good.id"
-              :good="good"
-              class="app-good-item"
-            />
-            <!-- </app-scroll> -->
-          </van-tab>
+            v-for="classify in classifyArr"
+            :title="classify.name"
+            :key="classify.classify_id"
+          ></van-tab>
         </van-tabs>
-      </app-scroll>
-      <!-- </div> -->
-    </div>
 
-    <!-- 子页面 -->
-    <transition enter-active-class="slideInRight" leave-active-class="slideOutRight">
-      <router-view></router-view>
-    </transition>
+        <!-- 内容滚动 -->
+        <app-scroll class="scrollContent">
+          <good-list :goodsListArr="goodsListArr" />
+        </app-scroll>
+
+        <!-- 子页面 -->
+        <transition enter-active-class="slideInRight" leave-active-class="slideOutRight">
+          <router-view></router-view>
+        </transition>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import commonService from "../../../services/commonService";
 import allService from "../../../services/allService";
-import GoodItem from "../../../components/app-good-item";
 import { Tab, Tabs } from "vant";
+import GoodList from "./children/Good-List";
 export default {
   name: "all",
   components: {
-    [Tab.name]: Tab,
     [Tabs.name]: Tabs,
-    [GoodItem.name]: GoodItem
+    [Tab.name]: Tab,
+    [GoodList.name]: GoodList
   },
   props: {},
   data() {
@@ -107,15 +100,12 @@ $mainColor: #ff1268;
     font-size: 46px;
   }
 }
-// .scrollContent {
-//   position: absolute;
-//   // top: 165px;
-//   // bottom: 0;
-//   left: 0;
-//   width: 100%;
-//   overflow: hidden;
-// }
-.app-good-item {
-  margin: 39px 0;
+
+.scrollContent {
+  position: absolute;
+  top: 154px;
+  left: 0;
+  bottom: 0;
+  width: 100%;
 }
 </style>
---
+++ src/pages/all/root/children/Good-List.vue
@@ -0,0 +1,33 @@
+<template>
+<div class="goodList">
+  <app-good-item v-for="good in goodsListArr" :key="good.id" :good="good" class="app-good-item" />
+</div>
+</template>
+
+<script>
+import GoodItem from "../../../../components/app-good-item";
+export default {
+  name: "good-list",
+  components: {
+    [GoodItem.name]: GoodItem
+  },
+  props: {
+    goodsListArr: {
+      type: Array,
+      default: []
+    }
+  },
+  data() {
+    return {};
+  },
+  computed: {},
+  watch: {},
+  methods: {}
+};
+</script>
+
+<style lang="scss">
+.app-good-item {
+  margin: 39px 0;
+}
+</style>
